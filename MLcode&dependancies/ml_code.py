5e1b5819dd1836f032ba98a6e939f9fef7a954b5
ZeyadM007 <61668904+ZeyadM007@users.noreply.github.com>
2021-01-23 15:37:06 -0500

    Create ml_code.py

---
+++ MLcode&dependancies/ml_code.py
@@ -0,0 +1,17 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.text import tokenizer_from_json
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import json
+
+def predictor(sentence):
+    sentence = [sentence]
+    with open('tokenizer.json') as f:
+        data = json.load(f)
+        tokenizer = tokenizer_from_json(data)
+    sequence = tokenizer.texts_to_sequences(sentence)
+    padded = pad_sequences(sequence, padding='post', maxlen=100, truncating='post')
+    model = load_model('fake_news_predictor.h5')
+    prediction = model.predict_classes(padded)
+    return prediction
+
+output = predictor('Trump impeachment trial to begin week of Feb. 8, Senate Democratic leader says')
