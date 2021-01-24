c1ec06902f0f4f1ff84e70c59edef99b46c42581
lucmaxwell <lucmaxwell8@gmail.com>
2021-01-23 20:10:25 -0600

    It works!!!!!!!!!

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, request, render_template, url_for
-#from ml_code import predictor
+from ml_code import predictor
 
 app = Flask(__name__)
-'''
-predictionNum = int(predictor('Trump is president'))
-
-result = ''
-if predictionNum == 1:
-    result = 'reliable'
-else:
-    result = 'unreliable'
-'''
 
 @app.route('/')
 def index():
-    return render_template('index.html', test="replace with result from ML file") 
+    return render_template('index.html', test="")
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    if text != "Simply Copy and Paste Your Headline Here an Press Submit~":
+        predictionNum = int(predictor('Trump is president'))
+        result = ''
+        if predictionNum == 1:
+            result = 'This source is reliable'
+        else:
+            result = 'This source is unreliable'
+        processed_text = text.upper()
+        return render_template('index.html', test=result)
+    else:
+        return render_template('index.html', test="")
 
 if __name__ == "__main__":
     app.run(debug = True)
