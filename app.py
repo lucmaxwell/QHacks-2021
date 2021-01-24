994641942a7e6e9bc5117c582e7298c9af5d24b0
lucmaxwell <lucmaxwell8@gmail.com>
2021-01-23 18:10:43 -0600

    push without ML model

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+#from ml_code import predictor
 
 app = Flask(__name__)
+"""
+predictionNum = int(predictor('Trump is president'))
+
+result = ''
+if predictionNum == 1:
+    result = 'reliable'
+else:
+    result = 'unreliable'
+"""
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #result = result
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
 
 if __name__ == "__main__":
     app.run(debug = True)
-
--- ml_code.py
+++ ml_code.py
@@ -7,14 +7,11 @@ import numpy as np
 
 def predictor(sentence):
     sentence = [sentence]
-    with open('MLmodel/tokenizer.json') as f:
+    with open('tokenizer.json') as f:
         data = json.load(f)
         tokenizer = tokenizer_from_json(data)
     sequence = tokenizer.texts_to_sequences(sentence)
     padded = pad_sequences(sequence, padding='post', maxlen=100, truncating='post')
-    model = load_model('MLmodel/fake_news_predictor.h5')
+    model = load_model('fake_news_predictor.h5')
     prediction = model.predict_classes(padded)
     return prediction[0][0]
-
-output = predictor('Trump impeachment trial to begin week of Feb. 8, Senate Democratic leader says')
-print(output)
