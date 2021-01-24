4843126f0a910d17cf1193560da0a20b15cb3f89
qynhan <qhan33@uwo.ca>
2021-01-23 17:07:57 -0800

    Got the logistics of connect ML to html

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for
 #from ml_code import predictor
 
 app = Flask(__name__)
-"""
+'''
 predictionNum = int(predictor('Trump is president'))
 
 result = ''
@@ -10,11 +10,11 @@ if predictionNum == 1:
     result = 'reliable'
 else:
     result = 'unreliable'
-"""
+'''
 
 @app.route('/')
 def index():
-    return render_template('index.html') #result = result
+    return render_template('index.html', test="replace with result from ML file") 
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -24,8 +24,3 @@ def my_form_post():
 
 if __name__ == "__main__":
     app.run(debug = True)
-<<<<<<< HEAD
-=======
-
-print(predictor('Trump is going to be a democrat'))
->>>>>>> 5184523f2b1f2287be8d43abd89b9e33b8aeb502
